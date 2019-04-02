e191e76f16d13d040adb21f78597ba0cd5d6e7b8
Expeditionary Interest Group (EIG) <45952339+eigproject1@users.noreply.github.com>
2019-04-02 12:53:04 -0400

    Add files via upload

---
+++ software/miner/ig/start-CPU.bat
@@ -0,0 +1,1 @@
+nheqminer -l zec-eu1.nanopool.org:6666 -u t1KBBBYniUBPCXC7DEtacePjwF1PJc2en6R/YOUR_WORKER_NAME/YOUR_EMAIL -p x
---
+++ software/miner/nvidia/start.bat
@@ -0,0 +1,1 @@
+miner --server zec-eu1.nanopool.org --user t1KBBBYniUBPCXC7DEtacePjwF1PJc2en6R.t --pass z --port 6666
